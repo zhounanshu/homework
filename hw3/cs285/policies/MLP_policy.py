18e9e1fd53f838e3d16abe900fbee2e9be19399d
marvin <1522684663@qq.com>
2023-03-05 23:21:49 +0800

    hw3-ac

--- hw3/cs285/policies/MLP_policy.py
+++ hw3/cs285/policies/MLP_policy.py
@@ -87,7 +87,13 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs: np.ndarray) -> np.ndarray:
         # TODO: get this from hw1 or hw2
-        return action
+        if len(obs.shape) > 1:
+            observations = obs
+        else:
+            observations = obs[None]
+        observations = ptu.from_numpy(observations)
+        actions = self(observations).sample()
+        return ptu.to_numpy(actions)
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -100,7 +106,19 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor):
         # TODO: get this from hw1 or hw2
-        return action_distribution
+        if self.discrete:
+            logits = self.logits_na(observation)
+            action_distribution = distributions.Categorical(logits=logits)
+        else:
+            batch_mean = self.mean_net(observation)
+            scale_tril = torch.diag(torch.exp(self.logstd))
+            batch_dim = batch_mean.shape[0]
+            batch_scale_tril = scale_tril.repeat(batch_dim, 1, 1)
+            action_distribution = distributions.MultivariateNormal(
+                batch_mean,
+                scale_tril=batch_scale_tril,
+            )
+            return action_distribution
 
 
 #####################################################
@@ -110,4 +128,17 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
 class MLPPolicyAC(MLPPolicy):
     def update(self, observations, actions, adv_n=None):
         # TODO: update the policy and return the loss
+        observations = ptu.from_numpy(observations)
+        actions = ptu.from_numpy(actions)
+        advantages = ptu.from_numpy(adv_n)
+
+        self.optimizer.zero_grad()
+        ac_dist = self(observations)
+        log_probs = ac_dist.log_prob(actions)
+
+        loss = -(log_probs * adv_n).sum()
+
+        loss.backward()
+        self.optimizer.step()
+        
         return loss.item()
