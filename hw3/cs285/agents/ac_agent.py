06dc7a8880111c51cdd3f83912933484aac12ec5
marvin <1522684663@qq.com>
2023-03-06 22:41:36 +0800

    hw3-ac

--- hw3/cs285/agents/ac_agent.py
+++ hw3/cs285/agents/ac_agent.py
@@ -34,12 +34,14 @@ class ACAgent(BaseAgent):
         # TODO Implement the following pseudocode:
         # for agent_params['num_critic_updates_per_agent_update'] steps,
         #     update the critic
-        critic_loss = self.critic.update(ob_no, ac_na, next_ob_no, re_n, terminal_n)
+        for _ in range(self.agent_params["num_critic_updates_per_agent_update"]):
+            critic_loss = self.critic.update(ob_no, ac_na, next_ob_no, re_n, terminal_n)
         # advantage = estimate_advantage(...)
         adv_n = self.estimate_advantage(ob_no, next_ob_no, re_n, terminal_n)
         # for agent_params['num_actor_updates_per_agent_update'] steps,
         #     update the actor
-        actor_loss = self.actor.update(ob_no, ac_na, adv_n)
+        for _ in range(self.agent_params["num_actor_updates_per_agent_update"]):
+            actor_loss = self.actor.update(ob_no, ac_na, adv_n)
         loss = OrderedDict()
         loss['Critic_Loss'] = critic_loss
         loss['Actor_Loss'] = actor_loss
